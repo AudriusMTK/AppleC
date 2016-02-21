f1cb331f19d91dd28e6131adc7ed79a14ee87d04
AudriusMTK <audriuslithuania@gmail.com>
2016-02-21 16:00:05 +0200

    Added files via upload
    Fixed

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
 package com.applec.spigot;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -13,20 +14,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class main extends JavaPlugin
 {
 	@Override
-	public boolean onCommand(CommandSender s, Command c,  String l, String[] args)
-	{
-		if(l.equalsIgnoreCase("obuolys"))
-		{
-			if (s instanceof Player)
-			{
-				Player p = (Player) s;
-					s.sendMessage(ChatColor.GREEN + "*" + ChatColor.AQUA + "Gavai savo obuolius" + ChatColor.GREEN + "*");
+	public boolean onCommand(CommandSender sender, Command cmd,  String label, String[] args){
+		if(label.equalsIgnoreCase("obuolys")){
+			if (sender instanceof Player){
+				Player p = (Player) sender;
+					sender.sendMessage(ChatColor.GREEN + "*" + ChatColor.AQUA + "Gavai savo obuolius" + ChatColor.GREEN + "*");
 					PlayerInventory i = p.getInventory();
 					i.addItem(new ItemStack(Material.APPLE, 16));
-					Bukkit.broadcastMessage(ChatColor.AQUA + s.getName() + ChatColor.BOLD + "gavo 16 obuoliu");
+					Location vieta = p.getLocation();
+					p.getWorld().playEffect(vieta, Effect.FLAME, 4);
 			return true;
-			}
+			} 
 		}
-		return false; 
-		}
-	}	
+		return false;
+	}
+}	
